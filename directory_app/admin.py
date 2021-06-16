52dbea21c3971ca3404c04674d29c5200300fc2d
HULI <doddahulugappa@gmail.com>
2021-06-16 10:12:24 +0400

    update subjects to display comma based

--- directory_app/admin.py
+++ directory_app/admin.py
@@ -5,6 +5,7 @@ from import_export.admin import ImportExportModelAdmin
 
 from django.core.exceptions import ValidationError
 from django import forms
+from django.utils.safestring import mark_safe
 
 
 class TeacherForm(forms.ModelForm):
@@ -21,16 +22,21 @@ class TeacherForm(forms.ModelForm):
 
 class TeacherAdmin(ImportExportModelAdmin):
     form = TeacherForm
-    list_display = ('first_name', 'last_name', 'email_address','profile_picture',)
+    raw_id_fields = ['subjects_taught']
+    list_display = ('first_name', 'last_name', 'email_address','profile_picture','subject_list')
 
 
     list_filter = ('first_name','last_name',)
 
     filter_horizontal = ['subjects_taught']
 
+    def subject_list(self,obj):
+        subject_list = ", ".join([x.subject_name for x in obj.subjects_taught.all()])
+        return mark_safe(subject_list)
+
 
 class SubjectAdmin(ImportExportModelAdmin):
-    list_display = ('subject_name',)
+    list_display = ['subject_name']
 
 
 admin.site.register(Subject, SubjectAdmin)
