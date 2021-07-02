24a702f1349a4c5cd403017f77c785475d6d61a9
HULI <doddahulugappa@gmail.com>
2021-07-02 15:29:37 +0400

    connected to postgres

--- directory_app/settings.py
+++ directory_app/settings.py
@@ -76,11 +76,32 @@ WSGI_APPLICATION = 'directory_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'teacher_directory',
+
+        'USER': 'huli@hulipostgres',
+
+        'PASSWORD': 'Admin123#',
+
+        'HOST': 'hulipostgres.postgres.database.azure.com',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
