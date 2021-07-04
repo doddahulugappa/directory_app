397f3ce51443b7297dc1f30bfa806cec99b560f2
HULI <doddahulugappa@gmail.com>
2021-07-04 23:07:52 +0400

    switched back to db sqlite3

--- directory_app/settings.py
+++ directory_app/settings.py
@@ -83,33 +83,33 @@ WSGI_APPLICATION = 'directory_app.wsgi.application'
 
 # Database
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'teacher_directory',
-
-        'USER': 'huli@hulipostgres',
-
-        'PASSWORD': 'Admin123#',
-
-        'HOST': 'hulipostgres.postgres.database.azure.com',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+
+
+# DATABASES = {
+#
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#
+#         'NAME': 'teacher_directory',
+#
+#         'USER': 'huli@hulipostgres',
+#
+#         'PASSWORD': 'Admin123#',
+#
+#         'HOST': 'hulipostgres.postgres.database.azure.com',
+#
+#         'PORT': '5432',
+#
+#     }
+#
+# }
 
 
 # Password validation
