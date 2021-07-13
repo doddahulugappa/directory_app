8e706da26fd8323f2f818af2f60c2dfbfbcd3705
HULI <doddahulugappa@gmail.com>
2021-07-13 14:23:24 +0400

    updated

--- directory_app/settings.py
+++ directory_app/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
